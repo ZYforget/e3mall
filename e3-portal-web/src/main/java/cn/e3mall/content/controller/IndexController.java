4a66961d789a3bbc01cf4fda8dfe40ee3799cddf
ZYforget <1659535230@qq.com>
2018-01-05 21:49:07 +0800

    Redis提交

--- e3-portal-web/src/main/java/cn/e3mall/content/controller/IndexController.java
+++ e3-portal-web/src/main/java/cn/e3mall/content/controller/IndexController.java
@@ -1,13 +1,30 @@
 package cn.e3mall.content.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.e3mall.content.service.ContentService;
+import cn.e3mall.pojo.TbContent;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	private ContentService contentService;
+
+	
+	@Value("${CONTENT_CATEGORYID}")
+	private long content_categoryId;
+	
 	@RequestMapping("/index")
-	public String showIndex(){
+	public String showIndex(Model model){
+		List<TbContent> list = contentService.indexShow(content_categoryId);
+		model.addAttribute("ad1List", list);
 		return "index";
 	}
 }
